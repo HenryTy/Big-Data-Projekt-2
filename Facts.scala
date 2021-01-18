34ca5fd48e35f5b3c002d17994a649d23731b0b8
Piotr Wojtyniak <33482377+HenryTy@users.noreply.github.com>
2021-01-18 22:20:42 +0100

    Update Facts.scala

--- Facts.scala
+++ Facts.scala
@@ -40,28 +40,30 @@ object Facts {
 
 
     // LOADING WEATHER
-    def getWeatherConditionsFromLine(line: String): String = {
-      val pattern = """^ of (.+) on (.+) at (.+) the following weather conditions were reported: (.+)$""".r
+   def getWeatherConditionsFromLine(line: String): String = {
+      val pattern = """^.+ of (.+) on (.+) at (.+) the following weather conditions were reported: (.+)$""".r
       line match {
         case pattern(local_authoirty_ons_code, date, time, conditions) => local_authoirty_ons_code + ";" + date + " " + time + ";" + conditions
         case _ => "None"
       }
     }
 
-    val weatherWithTime = spark.read.textFile(s"/user/$username/labs/spark/uk-traffic/weather.txt")
+    val weatherWithTime = spark.read.textFile(s"/user/ventus_piotrek/labs/spark/uk-traffic/weather.txt")
       .map(line => getWeatherConditionsFromLine(line))
-      .filter(!_.equals("None"))
       .withColumn("splitted", split($"value", ";"))
       .select(
         $"splitted".getItem(0).as("local_authoirty_ons_code").cast("string"),
-        $"splitted".getItem(1).as("timestamp"),
+        $"splitted".getItem(1).as("timestamp1"),
         $"splitted".getItem(2).as("conditions").cast("string")
       )
-      .withColumn("timestamp", to_timestamp($"timestamp", "dd/MM/yyyy HH:mm")) //todo w pliku jest bez sekund
+      .withColumn("timestamp", to_timestamp($"timestamp1", "dd/MM/yyyy HH:mm"))
       .withColumn("Year", functions.year(col("timestamp")))
       .withColumn("Month", functions.month(col("timestamp")))
       .withColumn("Day", functions.dayofmonth(col("timestamp")))
       .withColumn("Hour", hour(col("timestamp")))
+      .drop("timestamp1")
+      .drop("timestamp")
+      .distinct()
 
 
 
@@ -70,12 +72,15 @@ object Facts {
     val vehicle_types = Seq("pedal_cycles", "two_wheeled_motor_vehicles", "cars_and_taxis", "buses_and_coaches", "lgvs", "hgvs_2_rigid_axle", "hgvs_3_rigid_axle", "hgvs_4_or_more_rigid_axle", "hgvs_3_or_4_articulated_axle", "hgvs_5_articulated_axle", "hgvs_6_articulated_axle")
 
     val allTrafficWithTime = allTraffic.flatMap(r => vehicle_types.zipWithIndex.map(v =>
-      (r.getInt(0), r.getTimestamp(3), r.getInt(4), r.getString(5), r.getInt(v._2 + 17), v._1, r.getString(6), r.getString(7))))
-      .toDF("ID", "timestampDate", "hour", "local_authoirty_ons_code", "vehicle_count", "vehicle_type", "road_name", "road_category")
+      (r.getInt(0), r.getInt(2), r.getTimestamp(3), r.getInt(4), r.getString(5), r.getInt(v._2 + 17), v._1, r.getString(6), r.getString(7))))
+      .toDF("ID", "year", "timestampDate", "hour", "local_authoirty_ons_code", "vehicle_count", "vehicle_type", "road_name", "road_category")
 
 
     val allTrafficWithTimeAndWeather = allTrafficWithTime.join(weatherWithTime,
-      weatherWithTime("timestamp") === allTrafficWithTime("timestampDate") &&
+      weatherWithTime("Year") === allTrafficWithTime("year") &&
+      weatherWithTime("Month") === functions.month(allTrafficWithTime("timestampDate")) &&
+      weatherWithTime("Day") === functions.dayofmonth(allTrafficWithTime("timestampDate")) &&
+      weatherWithTime("Hour") === allTrafficWithTime("hour") &&
         weatherWithTime("local_authoirty_ons_code") === allTrafficWithTime("local_authoirty_ons_code")
     ).select(allTrafficWithTime("ID"), $"conditions")
 
@@ -83,7 +88,8 @@ object Facts {
     val timeDF = spark.sql("SELECT * FROM czas")
 
     val trafficTimes = allTrafficWithTime.join(timeDF,
-      to_timestamp(timeDF("data")) === allTrafficWithTime("timestampDate")
+      to_timestamp(timeDF("data")) === allTrafficWithTime("timestampDate") &&
+      timeDF("godzina") === allTrafficWithTime("hour")
     ).select(allTrafficWithTime("ID").as("id"), timeDF("id").as("id_czasu"))
 
 
