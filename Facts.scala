621ddf643a4a1b5d1fee33453a9985dde5c5eb14
Tomasz Jurek <48522246+tomaszmjurek@users.noreply.github.com>
2021-01-18 21:15:29 +0100

    Update Facts.scala

--- Facts.scala
+++ Facts.scala
@@ -57,7 +57,7 @@ object Facts {
         $"splitted".getItem(1).as("timestamp"),
         $"splitted".getItem(2).as("conditions").cast("string")
       )
-      .withColumn("timestamp", to_timestamp($"timestamp", "yyyy-MM-dd HH:mm:ss.S"))
+      .withColumn("timestamp", to_timestamp($"timestamp", "dd/MM/yyyy HH:mm")) //todo w pliku jest bez sekund
       .withColumn("Year", functions.year(col("timestamp")))
       .withColumn("Month", functions.month(col("timestamp")))
       .withColumn("Day", functions.dayofmonth(col("timestamp")))
@@ -70,7 +70,7 @@ object Facts {
     val vehicle_types = Seq("pedal_cycles", "two_wheeled_motor_vehicles", "cars_and_taxis", "buses_and_coaches", "lgvs", "hgvs_2_rigid_axle", "hgvs_3_rigid_axle", "hgvs_4_or_more_rigid_axle", "hgvs_3_or_4_articulated_axle", "hgvs_5_articulated_axle", "hgvs_6_articulated_axle")
 
     val allTrafficWithTime = allTraffic.flatMap(r => vehicle_types.zipWithIndex.map(v =>
-      (r.getInt(0), r.getTimestamp(3), r.getInt(4), r.getString(5), r.getInt(v._2 + 17), v._1, r.getString(6), r.getString(7))))  // to_timestamp(
+      (r.getInt(0), r.getTimestamp(3), r.getInt(4), r.getString(5), r.getInt(v._2 + 17), v._1, r.getString(6), r.getString(7))))
       .toDF("ID", "timestampDate", "hour", "local_authoirty_ons_code", "vehicle_count", "vehicle_type", "road_name", "road_category")
 
 
@@ -114,7 +114,7 @@ object Facts {
       .join(trafficLocation, trafficLocation("id") === trafficTimes("id"))
       .join(trafficTypes, trafficTypes("id") === trafficTimes("id"))
       .join(trafficWeather, trafficWeather("id") === trafficTimes("id"))
-      .select($"id_czasu", $"id_pojazdu", $"id_miejsca", $"id_pogody", trafficLocation("vehicle_count").as("liczba_pojazdow")) 
+      .select($"id_czasu", $"id_pojazdu", $"id_miejsca", $"id_pogody", trafficLocation("vehicle_count").as("liczba_pojazdow"))
 
 
     //    finalTable.show()
